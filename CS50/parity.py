700f43845a98eb185359b4cd7d3c64da9f2c84e7
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-08-01 00:29:03 +0530

    Find a number is odd or even

---
+++ CS50/parity.py
@@ -0,0 +1,12 @@
+# Program to find a
+# number is even or odd
+
+
+# Prompt user for input
+n = int(input("Enter the number: "))
+
+# Conditional
+if n % 2 == 0:
+    print("Even!")
+else:
+    print("Odd!")
