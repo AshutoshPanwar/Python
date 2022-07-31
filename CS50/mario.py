ec1187faa38c053da16e32c5b5a3dda9335d9ac7
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-08-01 00:36:29 +0530

    Print Horizontal wall of #

--- CS50/mario.py
+++ CS50/mario.py
@@ -1,28 +1,8 @@
 # Simple program to
-# print a vertical
+# print a horizontal
 # wall of (#)
 
+Height = int(input("Size: "))
 
-# Declaring main function
-def main():
-    height = get_height()       # Function call to get_height() function
-    for i in range(height):
-        print("#")
-
-
-# Function for user input
-def get_height():
-    # Repeat till condition is true
-    while True:
-        # Exception Handling
-        try:
-            height = int(input("Hight: "))
-            if height > 0:      # Condition
-                break
-        except ValueError:
-            print("That not an integer!")
-    return height               # Return Type
-
-
-# Program Start to Execute for here
-main()
+for i in range(Height):
+        print("#" * Height)
