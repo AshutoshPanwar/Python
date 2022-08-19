0fad27b6dc1719dca1cf428e2b83ad82f342d4b5
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-19 22:37:59 +0530

    moved folder to dir

---
+++ Real_Python/guess_the_password.py
@@ -0,0 +1,9 @@
+# Practical implementation 
+# of break to exit out of loop
+
+#! /usr/bin/pyhton3
+myStr = "Work most common password!"
+
+while myStr != "password":
+    myStr = input("Enter the 'password': ")
+
---
+++ return_multiple_values_form_func.py
@@ -0,0 +1,9 @@
+# Practical implementation
+# of tuples in python
+
+#! /usr/bin/pyhthon3
+
+def add_sub(num1,num2):
+    return (num1+num2,num1-num2)
+
+print(add_sub(10,5))
