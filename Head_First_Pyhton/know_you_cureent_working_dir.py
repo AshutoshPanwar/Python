5397eeebbf93d56432ca290c74399c7c821cc911
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-23 23:16:13 +0530

    Know your current working directory

---
+++ Head_First_Pyhton/know_you_cureent_working_dir.py
@@ -0,0 +1,11 @@
+# Program to know complete
+# path of your current working Dir
+
+# Import function from it's module
+from os import getcwd
+
+# The function define is standard module
+where_an_I = getcwd()
+
+# retur the path
+print(where_an_I)
