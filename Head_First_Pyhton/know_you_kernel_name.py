d2972205179dbb696e04537a4f1f13675815e05b
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-23 23:21:43 +0530

    Know you kernel name

---
+++ Head_First_Pyhton/know_you_kernel_name.py
@@ -0,0 +1,11 @@
+# Know the name of underline plateform
+# you are using on your machine
+
+# Import standard module
+import sys
+
+# Accessing plateform() function define in sys module
+Kernel = sys.platform
+
+# Return kernel name
+print(Kernel)
