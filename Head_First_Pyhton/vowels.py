a2244ce19616aedde3359206b08bbff531d90d8e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-25 01:50:43 +0530

    Function define with return type

--- Head_First_Pyhton/vowels.py
+++ Head_First_Pyhton/vowels.py
@@ -1,13 +1,13 @@
 # Program to print vowels
 # in word enter by the user
 
-# Functioin define with argument's
+# Functioin define with argument's & return type
 def search4vowels(word):
     """Display any vowel found in an asker-for word."""     # DocString
     vowel = set('aeiou')
     found = vowel.intersection(set(word))
     for vowel in found:
-        print(vowel)
+        return bool(found)
 
 # Function Call
-search4vowels('ashu')
+print(search4vowels('ashu'))
