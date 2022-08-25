4b18ae5ddaac7ad0c7941dde66abbbde9ba3164e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-25 23:56:23 +0530

    Data types!

---
+++ Udemy/Python_Mega_Course/exercise3.py
@@ -0,0 +1,17 @@
+from cgi import print_arguments
+
+
+i = 5       # Type is int
+f = 3.1415926       # Type is float
+string = "Pyhton"
+
+print(i)
+print(f)
+print(string)
+
+concatination = string + " " + string       # Result will be string
+print(concatination)
+
+sum = f + f     # Result will be float
+print(sum)
+
