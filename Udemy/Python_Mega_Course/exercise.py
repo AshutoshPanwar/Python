354c4cb7c59eb4bd7a0a577d87e8b10d940d59e0
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-25 23:34:08 +0530

    First exercise!

---
+++ Udemy/Python_Mega_Course/exercise.py
@@ -0,0 +1,17 @@
+# It will print new line after each work
+print("Hello")
+print("Python")
+
+# Print a new line
+print()
+
+# It will print a new line after print hello
+print("Hello", end='\n')
+print("World")
+
+# Print a new line
+print()
+
+# It will not print new line reather it will print space
+print("Hello", end=' ')
+print("Python")
