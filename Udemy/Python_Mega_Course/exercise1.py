74ab6cfef724080d0b87fba8866cb4aa1404aa9c
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-25 23:38:58 +0530

    Second exercise!

---
+++ Udemy/Python_Mega_Course/exercise1.py
@@ -0,0 +1,20 @@
+print(' ')
+print(" ")
+print("Hello world!")
+print("Hello          World")
+print("____________________")
+
+print()
+
+space = ' '
+print("Hello",space,"World!")
+print("Hello",space*5,"World!")
+print("Hello",space*10,"World!")
+
+print()
+
+space = "#"
+print("Hello",space,"World!")
+print("Hello",space*5,"World!")
+print("Hello",space*10,"World!")
+
