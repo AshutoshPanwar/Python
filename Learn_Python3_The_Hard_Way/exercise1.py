0d1371acd4b4f2c112dc63acec40bcc0c219d523
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-28 01:48:27 +0530

    First program of this book! about print()

---
+++ Learn_Python3_The_Hard_Way/exercise1.py
@@ -0,0 +1,7 @@
+print("Hello Python3:The Hard Way!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun!")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
