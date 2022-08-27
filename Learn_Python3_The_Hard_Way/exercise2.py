68728ceb5d660eff2551372734db26bb58896dda
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-08-28 01:53:24 +0530

    (#)Comments,Pound,Hash

---
+++ Learn_Python3_The_Hard_Way/exercise2.py
@@ -0,0 +1,7 @@
+# Comments and Pound Character
+# Anything after the # is ignored by python.
+
+print("Comments and Pound Character") # and the comment after is ignored
+
+# We also use comment to 'disable' the code
+# print("This will not run")
