ddea7e35637a9e955c471bd599bb74efe032bd0f
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-01 23:40:13 +0530

    More on calling , operatoins on variables

---
+++ Learn_Python3_The_Hard_Way/exercise5.py
@@ -0,0 +1,20 @@
+# More Variables and Printing
+
+# Variable deceleration
+my_name = 'Ashutosh Panwar'
+my_age = 20
+my_height = 5.7 # ft
+my_weight = 72 # kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+# Variable calling
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+
+# Operation on variables
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {my_age + my_height + my_weight}.")
