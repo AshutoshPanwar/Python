b0a889cdc1144e900f8f586a43555f54a40a717c
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-04 22:27:48 +0530

    Different way for passing and formatting string.

---
+++ Learn_Python3_The_Hard_Way/exercise9.py
@@ -0,0 +1,15 @@
+# Initializ variables
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan \nFeb \nMar \nApr \nMay \nJun \nJul \nAug"    # (\n) refers to nexl line.
+
+# Calling variables
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# Multi-line string
+print("""
+    There is something going on here.
+    With the three double-quotes.
+    We'll be able to type as much as we like.
+    Even 4 lines if we want, or 5, or 6.
+""")
