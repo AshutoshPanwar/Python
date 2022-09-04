1049b59d5f4042a1bb8083fe6c727dd4b0798eef
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-04 22:34:39 +0530

    User input without end=''

---
+++ Learn_Python3_The_Hard_Way/exercise12.py
@@ -0,0 +1,8 @@
+# Taking user input without end=''
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weight?")
+
+# Passing varibales with format()
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
