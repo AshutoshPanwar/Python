da754d2447822948694c455814b8acd1f53591c5
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-04 22:30:13 +0530

    Escape sequence in python

---
+++ Learn_Python3_The_Hard_Way/exercise10.py
@@ -0,0 +1,17 @@
+# Different escape Sequence
+
+taddy_cat = "\tI'm tabbed in."      # (\t) refers to tab
+persian_cat = "I'm split\non a line."       # (\n) refers to next line
+backslash_cat = "I'm \\ a \\ cat."      # (\\) refers to back slash
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(taddy_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
