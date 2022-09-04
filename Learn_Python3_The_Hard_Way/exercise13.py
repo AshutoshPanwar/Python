e6e129a8473ff3dcb84c6057e2f856e69cdaad73
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-04 22:36:36 +0530

    Command line arguments

---
+++ Learn_Python3_The_Hard_Way/exercise13.py
@@ -0,0 +1,11 @@
+# Taking run time input with file name
+# like python3 exercise13.py one two three
+
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script if called:", script)
+print("Your first variable is:", first)
+print("Your second variable is", second)
+print("Your third variable is", third)
