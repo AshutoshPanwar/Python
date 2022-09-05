202a3400c90a9029a8fc48a659e824a18880694e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-05 23:25:02 +0530

    Copying data from one file to another

---
+++ Learn_Python3_The_Hard_Way/exercise17.py
@@ -0,0 +1,29 @@
+# Copying data from one file to another
+
+from sys import argv
+from os.path import exists
+
+# Command line argumnets
+script,from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}. ")
+# Reading data from one file
+in_file = open(from_file)
+indata = in_file.read()
+
+# Size of data to copy
+print(f"The input file is {len(indata)} bytes long.")
+
+print(f"Does the output file exist? {exists(to_file)} ")
+print("Ready, hit RETURN to continue, CTRL-C to abort.")
+input()
+
+# Copying data to file
+out_file = open(to_file,'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+# Closing file
+out_file.close()
+in_file.close()
