5cc4d652212c032c2f0a6c5581c3726f6e7cf8ac
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-07 23:55:31 +0530

    if statement's

---
+++ Learn_Python3_The_Hard_Way/exercise29.py
@@ -0,0 +1,25 @@
+# Conditional statements
+
+# Variables
+people = 20
+cats = 30
+dogs = 15
+
+# if block will execute only if condition is true
+if people < cats:
+    print("Too many cats! The world is doomed!")
+if people > cats:
+    print("Not many cats! The world is saved!")
+if people < dogs:
+    print("The world is drooled on!")
+if people > dogs:
+    print("The world id dry!")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+if people == dogs:
+    print("People are dogs.")
