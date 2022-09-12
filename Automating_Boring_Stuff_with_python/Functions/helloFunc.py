5b2e333d51c68915f333934a1476d59b9462983c
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-12 23:23:20 +0530

    Functions in python

---
+++ Automating_Boring_Stuff_with_python/Functions/helloFunc.py
@@ -0,0 +1,9 @@
+# Function define
+def hello():
+    print("Hi user!")
+    print("I am Function!!!")
+    print("I am here to simplify you code!")
+
+# Function call
+hello()
+hello()
---
+++ Automating_Boring_Stuff_with_python/Functions/helloFunc2.py
@@ -0,0 +1,8 @@
+# Function define
+def hello(name):
+    print("Hello " + name)
+    print("I am parameterised function")
+
+# Function call
+hello('Ashu')
+hello("Jack")
