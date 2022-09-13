34982a47795dfd38cece02cbed58e70ee7627130
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-13 22:06:35 +0530

    Nested if-else

---
+++ Automating_Boring_Stuff_with_python/FlowControl/Nested_if_else.py
@@ -0,0 +1,11 @@
+# This program execure block of code if condition if true
+
+name = 'Ashu'
+password = 'BigBoss'
+
+if name == 'Ashu':
+    print("Hello, Ashu")
+    if password == 'BigBoss':
+        print("Access granted.")
+    else:
+        print("Wrong Password!")
