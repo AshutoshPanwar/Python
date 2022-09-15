b3f0bf3b150e1e8554003f870e3d60fcd0a15de4
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:19:01 +0530

    Life without List

---
+++ Automating_Boring_Stuff_with_python/Lists/allMyCats1.py
@@ -0,0 +1,17 @@
+# Life without List
+
+print("Enter the name of cat1:")
+cat1 = input()
+print("Enter the name of cat2:")
+cat2 = input()
+print("Enter the name of cat3:")
+cat3 = input()
+print("Enter the name of cat4:")
+cat4 = input()
+print("Enter the name of cat5:")
+cat5 = input()
+print("Enter the name of cat6:")
+cat6 = input()
+
+print("The cat names are: ")
+print(cat1 + ' ' + cat2 + ' ' + cat3 + ' ' + cat4 + ' ' + cat5 + ' ' + cat6 + ' ')
