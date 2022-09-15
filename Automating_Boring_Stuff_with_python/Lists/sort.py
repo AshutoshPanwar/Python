634e94ccbd075848db7fccdcd8be7c3a1cc1bade
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:37:10 +0530

    Sort a list with .sort()

---
+++ Automating_Boring_Stuff_with_python/Lists/sort.py
@@ -0,0 +1,11 @@
+# Sort a lsit with .sort()
+
+# Unsorted list
+people = ['Carol', 'David','Bob','Alice']
+
+print("Unsorted list")
+print(people)
+
+print("Sorted list:")
+people.sort()       #  .sort() will sort element in alphabetic order
+print(people)
