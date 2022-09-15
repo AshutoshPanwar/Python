13db13a809cbff17f48a426a5dd22c97c7effb0a
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:26:37 +0530

    .index() will return index value of item

---
+++ Automating_Boring_Stuff_with_python/Lists/findInList.py
@@ -0,0 +1,10 @@
+# .index() will return index of item in the list
+
+spam = ['hello', 'hi', 'howdy', 'hayes']
+
+print(spam)
+print("> Index of 'hi' in list is:")
+print(spam.index('hi'))     # .index() return the index of element is the list
+
+print("> Index of 'hayes' in list is:")
+print(spam.index('hayes'))     # .index() return the index of element is the list
