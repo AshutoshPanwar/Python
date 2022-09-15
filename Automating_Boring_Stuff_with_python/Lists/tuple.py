8eec974f631de70c4531154889f3f9a1c0fbcd9c
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:37:52 +0530

    tuples in python

---
+++ Automating_Boring_Stuff_with_python/Lists/tuple.py
@@ -0,0 +1,14 @@
+# Tuples are immutable
+# they are generally represented by ()
+
+eggs = ('hello', 'tuples', 'immutable')
+
+# Indexing with tuple
+print(eggs[0])
+print(eggs[2])
+
+# Slicing with tuple
+print(eggs[:2])
+print(eggs[:])
+
+print(type(eggs))
