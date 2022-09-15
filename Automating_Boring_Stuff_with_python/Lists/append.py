6e30e4a1eb7fe79ea5c79ce2108f903c33c333c8
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:22:03 +0530

    Add new element to list with append()

---
+++ Automating_Boring_Stuff_with_python/Lists/append.py
@@ -0,0 +1,12 @@
+# Add a new item to list with .append()
+
+spam = ['cat', 'dog', 'bat']
+
+# See current elements in list
+print("Elements in the list:")
+print(spam)
+
+spam.append('BigBull')      # Adding new element to list
+# Shol list after adding new element
+print("After adding new element to list:")
+print(spam)
