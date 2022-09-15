10b587496a526d5dcc8957821dac40e6dfbd4285
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:36:32 +0530

    Reverse a list with .reverse()

---
+++ Automating_Boring_Stuff_with_python/Lists/reverse.py
@@ -0,0 +1,10 @@
+# Revese a list with reverse func
+
+people = ['carol', 'David','bob','Alice']
+
+print('Before reversing list')
+print(people)
+
+print("After reversing list")
+people.reverse()        # .reverse() will return reversed list
+print(people)
