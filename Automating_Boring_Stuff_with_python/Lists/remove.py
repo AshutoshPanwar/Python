6c0434638358eaf74edae6304dbb2b0d1f8cf6f6
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:35:22 +0530

    Removing item from list

---
+++ Automating_Boring_Stuff_with_python/Lists/remove.py
@@ -0,0 +1,10 @@
+# Removing item from list
+
+spam = ['cat', 'dog', 'bat']
+
+print("Before removing the elements.")
+print(spam)
+
+print('After removing element.')
+spam.remove('dog')      # remove is used when you know the value
+print(spam)
