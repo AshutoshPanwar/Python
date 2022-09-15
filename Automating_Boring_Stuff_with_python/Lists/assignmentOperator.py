2bcac448f2bc606ecd26a20ddd41a65fcb79029a
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-15 22:22:43 +0530

    Assigment operator(+=, -=, *=, /=, %=)

---
+++ Automating_Boring_Stuff_with_python/Lists/assignmentOperator.py
@@ -0,0 +1,28 @@
+spam = 2
+# Naive method
+spam = spam + 1
+print(spam)
+
+
+spam = 2
+# Python style method
+spam += 1
+print(spam)
+
+
+# Some common operators
+spam = 2
+spam -= 1       # spam = spam - 1
+print(spam)
+
+spam = 2
+spam *= 1       # spam = spam * 1
+print(spam)
+
+spam = 2
+spam /= 1       # spam = spam / 1
+print(spam)
+
+spam = 2
+spam %= 1       # spam = spam % 1
+print(spam)
