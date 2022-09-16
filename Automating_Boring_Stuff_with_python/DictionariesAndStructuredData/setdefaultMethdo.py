bf700eae237de4d07a5b0676278ef348af179273
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-16 22:34:44 +0530

    .setdefault(keyname, value) method

---
+++ Automating_Boring_Stuff_with_python/DictionariesAndStructuredData/setdefaultMethdo.py
@@ -0,0 +1,10 @@
+spam = {'name' : 'Pooka', 'age' : 5}
+
+print('Element in list:')
+print(spam)
+
+# dictionary.setdefault(keyname, value)
+spam.setdefault('color', 'black')       # Adding new element
+
+print("After adding new element:")
+print(spam)
