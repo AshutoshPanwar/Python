07c66c6599a139abd00abc474c741ffe8751154b
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-16 22:33:18 +0530

    .get(keyname, value()) method

---
+++ Automating_Boring_Stuff_with_python/DictionariesAndStructuredData/getMethod.py
@@ -0,0 +1,6 @@
+picnincItems = {'apple' : 5, 'cups' : 2}
+
+# dictionary.get(keyname, value(optional)-> will used when value not found)
+print("I am bringing " + str(picnincItems.get('cups', 0)) + ' cups.')
+print("I am bringing " + str(picnincItems.get('eggs', 0)) + ' cups.')
+
