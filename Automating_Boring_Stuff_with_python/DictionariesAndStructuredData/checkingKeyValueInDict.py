5ef0f8439ff4d7bcf6b04113096e28fbedf6410f
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-16 22:31:27 +0530

    .key(), .value() Dict methods

---
+++ Automating_Boring_Stuff_with_python/DictionariesAndStructuredData/checkingKeyValueInDict.py
@@ -0,0 +1,12 @@
+spam = {'name' : 'Ashu', 'age' : 20}
+
+# See Dict key-Value
+print(spam)
+
+# Checking 'name' exist as key in Dict
+print("(name) in spam.keys() :")
+print('name' in spam.keys())
+
+# Checking 'Ashu' exit as value in Dict
+print("(Ashu) in spam.values(): ")
+print("Ashu" in spam.values())
