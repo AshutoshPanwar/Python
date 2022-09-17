cbc6efc8cddbbf9519b5e625a5b0adf9300246ae
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-17 23:17:18 +0530

    Function with return operation

---
+++ Real_Python/return_multiple_values_form_func.py
@@ -0,0 +1,10 @@
+#! /usr/bin/pyhthon3
+
+# Parameterised function with return operation
+
+# Function define
+def add_sub(num1,num2):
+    return (num1+num2,num1-num2)
+
+# Function call
+print(add_sub(10,5))
