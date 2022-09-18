7cbbca1e775c66ef9477f12628223b8c9c32e60c
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-18 23:46:48 +0530

    Multi-Line comment's

---
+++ Automating_Boring_Stuff_with_python/ManipulatingStrings/multiLineComment.py
@@ -0,0 +1,13 @@
+# (""" """) are used for multiLine comment's
+
+print("""This is a test python program.
+    Written by Ashutosh Panwar.
+    This program is designed for python3 not python2.""")
+
+def spam():
+    """This is a multil line comment used to help
+    explain what the spam() function does.
+    """
+    print('Hello!')
+
+print(spam())
