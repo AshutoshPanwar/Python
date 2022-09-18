c651adf070ac307fa8d96d0054020e1a89688e8e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-18 23:44:15 +0530

    Escaper Sequence in python

---
+++ Automating_Boring_Stuff_with_python/ManipulatingStrings/escapeCharacter.py
@@ -0,0 +1,8 @@
+# Escape Character's
+# \n    NewLine(line break)
+# \'    Single quote
+# \"    Double quote
+# \t    Tab
+# \\    BackSlash
+
+print("Hello there!\nHow are you?\nI\'m doing fine.")
