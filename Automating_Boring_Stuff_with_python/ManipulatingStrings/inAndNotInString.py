4f6ac16828667b3bd55ddc457e4402d4ea981d66
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-18 23:46:27 +0530

    Check sub-string in a string with (in, not in)

---
+++ Automating_Boring_Stuff_with_python/ManipulatingStrings/inAndNotInString.py
@@ -0,0 +1,13 @@
+# Check a sub-string exist in string or not with (in), (not in)
+
+print("Hello in Hello, pyhton3! :")
+print("Hello" in 'Hello, pyhton3!')
+
+print("Hello in Hello :")
+print("Hello" in "Hello")
+
+print("HELLO in Hello World!:")
+print("HELLO" in "Hello World!")
+
+print("cats not in cats and dogs:")
+print("cats" not in 'cats and dogs')
