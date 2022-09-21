da394fb0868b018252d5ff35d1c348f9d53880ac
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-09-21 21:55:42 +0530

    Creating, accessing variable to other file

---
+++ Automating_Boring_Stuff_with_python/ReadingAndWritingFiles/myCats.py
@@ -0,0 +1,4 @@
+# This file created using python
+# This file can be import to other python file with import myCats
+# cats variables can be accessed with myCats.cats
+cats = [{'desc': 'chuddy', 'name': 'Zophie'}, {'desc': 'fluffy', 'name': 'pooka'}]
