195eef26f006ac76286e3fe7d9ea4755bd7aa490
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-10-01 23:35:12 +0530

    Read CSV file, items

--- Automating_Boring_Stuff_with_python/WorkingWithCSVandJSON/ReadingCSV.py
+++ Automating_Boring_Stuff_with_python/WorkingWithCSVandJSON/ReadingCSV.py
@@ -5,3 +5,10 @@ exampleRead = csv.reader(exampelFile)       # Read CSV
 exampleData = list(exampleRead)         # Convert data to list format
 
 print(exampleData)      # Print data
+
+print("Accessing specific data:")
+print(exampleData[0][0])
+print(exampleData[1][1])
+print(exampleData[1][2])
+print(exampleData[2][1])
+print(exampleData[6][1])
