79389b49b6e6778a9212bbde8ad86cb22c3d7763
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-10-13 22:12:16 +0530

    Tick Tock count down

---
+++ Automating_Boring_Stuff_with_python/WorkingWithCSVandJSON/KeepingTime-SchedulingTasks-LauncingPrograms/TimeSleep.py
@@ -0,0 +1,7 @@
+import time
+
+for i in range(5):
+    print('Tick')
+    time.sleep(1)       # Wait for 1 sec and then execute next command
+    print('Tock')
+    time.sleep(1)       # Wait for 1 sec and then execute next command
