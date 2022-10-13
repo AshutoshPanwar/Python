0193f5e9220ee1df0d1627466dde3221e191279e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-10-13 22:12:45 +0530

    Know Date Time and access it's attributes

---
+++ Automating_Boring_Stuff_with_python/WorkingWithCSVandJSON/KeepingTime-SchedulingTasks-LauncingPrograms/DateTimeModule.py
@@ -0,0 +1,13 @@
+import datetime
+
+# Know current date-Time
+print(datetime.datetime.now())
+
+# Accessing specific value of datatime module
+dt = datetime.datetime.now()
+print(dt.year)      # Know year
+print(dt.month)     # know month
+print(dt.day)       # know day
+print(dt.hour)      # know hour
+print(dt.minute)    # know minutes
+print(dt.second)    # know seconds
