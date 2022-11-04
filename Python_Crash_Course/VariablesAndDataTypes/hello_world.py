26f8f86533d3dcaf87fb62c78859cdb2253cb60e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-04 22:30:14 +0530

    Variable Behavior

---
+++ Python_Crash_Course/VariablesAndDataTypes/hello_world.py
@@ -0,0 +1,6 @@
+message = 'Hello Python World!'     # Added Variable name message
+print(message)
+
+# We can change value in program any time.
+message = 'Hello Python Crash Corse World!'
+print(message)
