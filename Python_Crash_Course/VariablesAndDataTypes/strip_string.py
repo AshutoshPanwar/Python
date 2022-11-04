786c07d05aba8194c5cafb64ff4ed2a0c7b14454
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-04 23:02:03 +0530

    Remove Extra White Spaces from string.

---
+++ Python_Crash_Course/VariablesAndDataTypes/strip_string.py
@@ -0,0 +1,8 @@
+# Remove extra white spaces form string
+# rstrip -> remove white space form right
+# lstrip -> remove white space form left
+# strip -> remove white space form both side
+
+favorite_language = 'python  '
+print(favorite_language)
+print(favorite_language.rsplit())
