df06cda8be16dfc78833dddc5334ea6e8f5573e2
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 03:21:31 +0530

    Tuples and it's properties

---
+++ Python_Crash_Course/WorkingWithList/tuples.py
@@ -0,0 +1,13 @@
+# Tuples in Python
+# Items in a Tuple can't be modified
+
+dimensions = (200, 50)
+print("Original Dimension:")
+for dimension in dimensions:
+    print(dimension)
+
+# Complete Tuple can be Modified.
+dimensions = (400, 100)
+print("\nModofied Dimension:")
+for dimension in dimensions:
+    print(dimension)
