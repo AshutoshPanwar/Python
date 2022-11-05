788153d30b4edcc0a0ef756038a331e709a8974e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-05 15:39:14 +0530

    Sort item in a list.

---
+++ Python_Crash_Course/IntroducingList/sort_list.py
@@ -0,0 +1,12 @@
+# Sort() item of a list
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+# Sort item is Acending order
+cars.sort()
+print(cars)
+
+# Sotr item in Decending order
+cars.sort(reverse=True)
+print(cars)
