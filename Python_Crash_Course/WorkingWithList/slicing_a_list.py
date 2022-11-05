0a789344e8e216677e7949e2d0e9898c2c7ab359
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 03:17:01 +0530

    Slice a List

---
+++ Python_Crash_Course/WorkingWithList/slicing_a_list.py
@@ -0,0 +1,6 @@
+# Slicing a list[start:stop]
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print(players[0:3])     # First three items
+print(players[-3:])     # Last three items
