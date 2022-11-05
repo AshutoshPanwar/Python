fc9a1ab687e2e9b43eeb3d78e3a5399ca8819b7d
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-05 15:39:49 +0530

    Temp store sorted list.

---
+++ Python_Crash_Course/IntroducingList/sorted_list.py
@@ -0,0 +1,12 @@
+# Sorting a list Temporatily with the sorted() Function.
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("Here is the sorted list:")
+print(sorted(cars))
+
+print("Here is the original list:")
+print(cars)
