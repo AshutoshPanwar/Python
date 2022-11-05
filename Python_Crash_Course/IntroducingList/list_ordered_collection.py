601e9b4afc630606334c08c70f04dfd6113fbdd4
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-05 15:37:46 +0530

    Accessing list item is specific order.

---
+++ Python_Crash_Course/IntroducingList/list_ordered_collection.py
@@ -0,0 +1,7 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialization']
+
+# List is a ordered collection, which means we can access any element by telling it's index.
+# Indexing starts from 0.
+print(bicycles[0])
+print(bicycles[3])
+print(f"My first bicycle was a {bicycles[0].title()}.")
