e21fa93c73e4a3c15546d6ec28291f0b89b8693f
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-05 15:37:07 +0530

    Adding new items to list

---
+++ Python_Crash_Course/IntroducingList/adding_elements_to_list.py
@@ -0,0 +1,12 @@
+# Appending elements to the end of list.
+motorcycles = ['honda', 'Royal Enfield', 'BMW']
+print(motorcycles)
+
+motorcycles.append('Ducati')
+print(motorcycles)
+
+
+# Inserting element into a specific positon of a list.
+motorcycles = ['honda', 'Royal Enfield', 'BMW']
+motorcycles.insert(0, 'KTM')
+print(motorcycles)
