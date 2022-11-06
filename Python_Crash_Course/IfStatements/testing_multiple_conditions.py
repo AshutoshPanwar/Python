64c4126ebd6486dfd55c4eea9678129331a68572
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 23:52:21 +0530

    Testing multiple conditions

---
+++ Python_Crash_Course/IfStatements/testing_multiple_conditions.py
@@ -0,0 +1,11 @@
+# Testing multiple conditions
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'corn' in requested_toppings:
+    print("Adding Corn.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
