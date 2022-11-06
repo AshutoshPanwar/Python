718005db9b282fa0b9beb75675a7bd24afca38dc
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 23:51:33 +0530

    if-else conditionals

---
+++ Python_Crash_Course/IfStatements/if-else_statement.py
@@ -0,0 +1,11 @@
+# if-else conditionals
+
+print("Enter you age: ")
+age = int(input())
+
+if age >= 18:
+    print("You are old enought to vote!")
+    print("Have you register to vote yet?")
+else:
+    print("Sorry, You are too yount to vote.")
+    print("Please register to vote as soon as you turned 18!")
