a32a2eab410cabc6e22d5bbe4979b5db5faa82c5
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 23:57:13 +0530

    Adding new Key-Value pairs to Dictionaries

---
+++ Python_Crash_Course/Dictionaries/adding_new_Key-value_pairs.py
@@ -0,0 +1,11 @@
+# Adding new key-value pairs to Dictionaries
+
+alien_0 = {'color':'green', 'points':5}
+print("Original Dictionary:")
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print("New Dictionary:")
+print(alien_0)
