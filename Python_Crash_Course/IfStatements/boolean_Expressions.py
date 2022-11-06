a82e9aa464a1b5d6f7fb3c7c84a8b700e62c4e4f
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 23:49:21 +0530

    Boolean Comparison (==)

---
+++ Python_Crash_Course/IfStatements/boolean_Expressions.py
@@ -0,0 +1,7 @@
+# Boolean Expression (==)
+car = 'subaru'
+print("Is car == 'subaru'? I predicted True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predicted False.")
+print(car == 'Audi')
