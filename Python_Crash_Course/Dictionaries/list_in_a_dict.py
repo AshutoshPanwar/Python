525a7019c522b8b75615533464937b32d60ae791
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-07 00:00:59 +0530

    List inside Dictionaries.

---
+++ Python_Crash_Course/Dictionaries/list_in_a_dict.py
@@ -0,0 +1,11 @@
+# Using list inside Dictionaries.
+
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+print(f"You ordered a {pizza['crust']}-crust pizza.")
+
+for topping in pizza['toppings']:
+    print('\t' + topping)
