f40f1f1e307a5408db7255e49b9baeb4e9b14bed
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-07 00:00:20 +0530

    Dictionaries of similar objects

---
+++ Python_Crash_Course/Dictionaries/dictionary_of_similar_objects.py
@@ -0,0 +1,11 @@
+# Dictionaries of similar objects
+
+fav_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+language = fav_language['phil'].title()
+print(f"phil favorite language is {language}.")
