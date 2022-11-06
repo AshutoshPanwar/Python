5439c8829d97ae591ca115d5c1fa1012e8bce620
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 23:49:59 +0530

    if-else with loop

---
+++ Python_Crash_Course/IfStatements/conditional_loop.py
@@ -0,0 +1,9 @@
+# Print bmw in upper case and all other in title case.
+
+cars = ['audo', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
