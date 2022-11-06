04710b8ff7cc3a08139d611ce5ea91bdf4a15d48
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-06 23:53:54 +0530

    Multiple list's with if-else

---
+++ Python_Crash_Course/IfStatements/using_multiple_list.py
@@ -0,0 +1,11 @@
+# Using multiple list's with if-else
+
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}")
+    else:
+        print(f"Sorry, we don't have {requested_topping}")
