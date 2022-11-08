b116a74a7271c1300bdfa355eada934401684db2
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-08 21:38:02 +0530

    While Loop

---
+++ Python_Crash_Course/UserInputAndWhileLoops/while_loop.py
@@ -0,0 +1,7 @@
+# While loop is used when we don't know the number of iterations.
+
+current_number = 1      # Pointer to keep track of no of iterations.
+
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
