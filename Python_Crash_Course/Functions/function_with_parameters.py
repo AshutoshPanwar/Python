0aee754120bd14c38bf92261dbdae5c7ebf33fd9
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-08 21:50:03 +0530

    Parameterised Function

---
+++ Python_Crash_Course/Functions/function_with_parameters.py
@@ -0,0 +1,9 @@
+# Function Define
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello {username.title()}")
+
+# Function call
+greet_user('ashu')
+greet_user('jack')
+greet_user('cobre')
