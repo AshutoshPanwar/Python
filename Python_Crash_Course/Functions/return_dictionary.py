36e083e55892a073e70f9520932a17f29b92eb49
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-08 22:00:05 +0530

    Function return Dictionary

---
+++ Python_Crash_Course/Functions/return_dictionary.py
@@ -0,0 +1,11 @@
+# Function return Dictionary
+
+def build_person(first_name, last_name, age=None):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('ashu', 'panwar', 25)
+print(musician)
