9939d91b19759977f5a37146e827960f69b9736e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-08 21:46:26 +0530

    Creating and Importing Modules

---
+++ Python_Crash_Course/Functions/making_pizza.py
@@ -0,0 +1,5 @@
+# Importing self created module
+
+import pizza_module
+
+pizza_module.make_pizza(16, 'mushroom', 'green peppers', 'extra cheese')
---
+++ Python_Crash_Course/Functions/using_as_to_give_a_module_an_alias.py
@@ -0,0 +1,5 @@
+# Using alis for self created module.
+
+import pizza_module as pz
+
+pz.make_pizza(16, 'mushroom', 'green peppers', 'extra cheese')
