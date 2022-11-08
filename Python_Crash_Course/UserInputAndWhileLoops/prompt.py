c4dcca1b5b338960405354c374d5a677bfef6b0e
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-08 21:39:28 +0530

    Prompt user to enter required input

---
+++ Python_Crash_Course/UserInputAndWhileLoops/prompt.py
@@ -0,0 +1,7 @@
+# Showing how to write a prompt.
+
+prompt = "If you tell us who you are, we can personalize the message you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
