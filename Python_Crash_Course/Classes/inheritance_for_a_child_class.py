f02141683c2e8a74f21404e7a3b3f1d0014ba97a
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-13 22:22:38 +0530

    Inheritance

--- Python_Crash_Course/Classes/inheritance_for_a_child_class.py
+++ Python_Crash_Course/Classes/inheritance_for_a_child_class.py
@@ -1,5 +1,6 @@
 # Inheritance
 
+# Parent Class
 class Car:
     """A simple attemp to represent a car."""
     def __init__(self, make, model, year):
@@ -25,10 +26,16 @@ class Car:
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
+# Child Class
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery_size = 75
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-KWH battery.")
 
 my_tesla = ElectricCar('tesla', 'model S', 2019)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
